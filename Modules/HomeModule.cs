53d2db94c8273c85bcf9566632caee11e944e502
Charlie Baxter and Sami Al-Jamal <charlie+sami@>
2016-07-19 14:07:06 -0700

    add routes to homemodule for views of all & individual courses and students

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,43 @@ namespace Registrar
   {
     public HomeModule()
     {
+      Get["/"]=_=> {
+        return View["index.cshtml"];
+      };
+
+      Get["courses/all"] =_=> {
+        List<Course> allCourses = Course.GetAll();
+        return View["courses.cshtml", allCourses];
+      };
+
+      Get["/courses/{id}"] = parameters =>{
+        Course selectedCourse = Course.Find(parameters.id);
+        return View["course.cshtml", selectedCourse];
+      };
+
+      Post["/courses/new"] = _ => {
+        Course newCourse = new Course(Request.Form["course-teacher"], Request.Form["course-name"]);
+        newCourse.Save();
+        List<Course> allCourses = Course.GetAll();
+        return View["courses.cshtml", allCourses];
+      };
+
+      Get["/students/all"] =_=> {
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
+
+      Get["/students/{id}"] = parameters => {
+        Student selectedStudent = Student.Find(parameters.id);
+        return View["student.cshtml", selectedStudent];
+      };
+
+      Post["/students/new"] = _ => {
+        Student newStudent = new Student(Request.Form["student-name"]);
+        newStudent.Save();
+        List<Student> allStudents = Student.GetAll();
+        return View["students.cshtml", allStudents];
+      };
     }
   }
 }
